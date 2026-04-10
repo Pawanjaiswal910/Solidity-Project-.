d83c032e5231f466dedb42a089287aed0fdef975
Pawan Jaiswal <paeanjaiswal428@gmail.com>
2026-04-10 17:30:25 +0530

    Add files via upload
    💻 Solidity Deep Dive: Inheritance & Multiple Inheritance Resolution
    
    I implemented contract inheritance in Solidity to understand how function overriding and multiple inheritance work.
    
    🔧 Features:
    • Base contract with virtual functions
    • Function overriding using override keyword
    • Multiple inheritance across contracts (B, C, D, E, F)
    • Usage of super to resolve parent function calls
    
    📚 Key Concepts:
    • Solidity’s inheritance linearization (right-to-left, depth-first)
    • Method resolution order (MRO)
    • Writing modular and reusable smart contracts
    • Avoiding conflicts in multiple inheritance
    
    This strengthens my foundation in building scalable and maintainable Web3 systems.
    
    #Solidity #Ethereum #Web3Development #BlockchainDeveloper

---
+++ Inheritance.sol
@@ -0,0 +1,69 @@
+      // How to create inheritance in solidity From Parent contract.
+
+      //SPDX-License-Identifier:MIT
+
+      pragma solidity ^0.8.13;
+
+    /*  Graph of inheritance 
+        A 
+       / \
+      B   C 
+     / \  / 
+     F  D,E 
+     */
+
+     contract A {
+        function foo() public pure virtual returns  (string memory) {
+            return "A";
+
+        }
+     }
+
+     // contracts inherit other contracts by using the keyword 'is'.
+
+     contract B is A {
+        //Override A.foo()
+        function foo() public pure virtual override returns (string memory) {
+            return "B";
+
+        }
+     }
+     contract C is A {
+        //Override A.foo()
+        function foo() public pure virtual override returns (string memory) {
+            return "C";
+        }
+     }
+
+     //contracts can  inherit from multiple parent contracts
+     //When a function is called that is defined multiple time in 
+     //different contracts , parent  contracts are searched from 
+     //right to left , and in depth - first manner.
+
+     contract D is B,C {
+        // D.foo () returns "C"
+        // since c is the right most parent contract with function foo()
+        
+        function foo() public pure override(B,C) returns (string memory) {
+            return super.foo();
+
+        }
+     }
+
+     contract E is C,B {
+        // E.foo() returns "B"
+        // since B is the right most parent contract with function foo()
+        function foo() public pure override(C,B) returns (string memory){
+            return super.foo();
+
+        }
+     }
+
+     //Inheritance must be ordered from "most base-like" to "most derived".
+     //Swapping the order of A and B will throw a compilation error.
+
+     contract F is A,B {
+        function foo() public  pure override(A,B) returns  (string memory) {
+            return super.foo();
+        }
+     }
